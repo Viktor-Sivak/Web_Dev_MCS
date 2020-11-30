af4e3678f3bf41ce3ddf0d7b70a3a19ccb9967c4
Viktor-Sivak <victorsiv@yandex.ru>
2020-11-30 17:21:24 +0300

    3.1 practice

---
+++ js/03.1_third_practice/js/script.js
@@ -0,0 +1,8 @@
+let whatNumber = parseInt (prompt ("Узнать модуль числа"));
+whatNumber = parseInt (whatNumber);
+
+if (whatNumber>0) {
+	console.log(1 * whatNumber)
+}else if (whatNumber<0) {
+	console.log(-1 * whatNumber)
+}
