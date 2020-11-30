3feff204ec20b7612455fb6d67594ace4337e9c9
Viktor-Sivak <victorsiv@yandex.ru>
2020-11-30 20:16:41 +0300

    Update script.js

--- js/03.1_third_practice/js/script.js
+++ js/03.1_third_practice/js/script.js
@@ -1,10 +1,3 @@
 let whatNumber = parseInt (prompt ("Узнать модуль числа"));
 whatNumber = parseInt (whatNumber);
-
-if (whatNumber>0) {
-	console.log(1 * whatNumber)
-}else if (whatNumber<0) {
-	console.log(-1 * whatNumber)
-}if (whatNumber==0) {
-	console.log (0)
-}
+(whatNumber>=0) ? console.log(1 * whatNumber) : console.log(-1 * whatNumber);
