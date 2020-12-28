8e818c4e7ce6175dd11d6881c8b21660d97b75eb
Viktor-Sivak <victorsiv@yandex.ru>
2020-12-28 18:48:39 +0300

    update 29.12

--- partisan-press/01_main_page_rebuild/js/script.js
+++ partisan-press/01_main_page_rebuild/js/script.js
@@ -67,3 +67,27 @@ menuBtn.addEventListener('click', function(){
 	menu.classList.toggle('active');
 })
 
+
+var linkNav = document.querySelectorAll('[href^="#to-store"]'), //выбираем все ссылки к якорю на странице
+    V = .1;  // скорость, может иметь дробное значение через точку (чем меньше значение - тем больше скорость)
+for (var i = 0; i < linkNav.length; i++) {
+    linkNav[i].addEventListener('click', function(e) { //по клику на ссылку
+        e.preventDefault(); //отменяем стандартное поведение
+        var w = window.pageYOffset,  // производим прокрутка прокрутка
+            hash = this.href.replace(/[^#]*(.*)/, '$1');  // к id элемента, к которому нужно перейти
+        t = document.querySelector(hash).getBoundingClientRect().top,  // отступ от окна браузера до id
+            start = null;
+        requestAnimationFrame(step);  // подробнее про функцию анимации [developer.mozilla.org]
+        function step(time) {
+            if (start === null) start = time;
+            var progress = time - start,
+                r = (t < 0 ? Math.max(w - progress/V, w + t) : Math.min(w + progress/V, w + t));
+            window.scrollTo(0,r);
+            if (r != w + t) {
+                requestAnimationFrame(step)
+            } else {
+                location.hash = hash  // URL с хэшем
+            }
+        }
+    }, false);
+}
